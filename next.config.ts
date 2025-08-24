eaa97fd44f25fb78d2de936ecaeb788ce02749f8
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2025-08-24 19:02:59 +0530

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // eslint: {
-  //   // Warning: This will skip ESLint during production builds
-  //   ignoreDuringBuilds: true,
-  // },
+  eslint: {
+    // Warning: This will skip ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
   // typescript: {
   //   // Warning: This will allow builds even with type errors
   //   ignoreBuildErrors: true,
